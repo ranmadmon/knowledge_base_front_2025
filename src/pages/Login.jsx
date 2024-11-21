726910927455da9887dd7e09bc9f5c44de6c33a5
Owner <elon9802@gmail.com>
2024-11-21 13:23:32 +0200

    login and register page update

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,11 +1,44 @@
 import { useState } from "react";
 import { useNavigate } from 'react-router-dom';
 import './Login.css';
+import axios from "axios";
 
 function Login() {
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
+    const [errorCode, setErrorCode]= useState(-1);
+
     const navigate = useNavigate();
+
+    const SERVER_URL = "http://localhost:8080"
+    const ERROR_PASSWORD = 401;
+    const USER_NOT_EXIST = 400;
+
+    function showErrorCode(){
+
+        let errorMessage = "";
+        switch (errorCode){
+
+            case -1 : errorMessage = "Please fill in all fields"; break;
+            case  USER_NOT_EXIST :errorMessage = "Not exist, SIGN-UP 😁";break;
+            case ERROR_PASSWORD : errorMessage = "Error password";break;
+        }
+        return errorMessage;
+    }
+
+    function login(){
+        axios.get(SERVER_URL+"/login?username=" + username + "&password=" + password)
+            .then(response => {
+                if (response.data.success){
+                    if (!response.data.loginSuccessful){
+                        setErrorCode(response.data.errorCode)
+                    }else{
+                        navigate("/dashboard");
+                    }
+                }
+            })
+    }
+
     return (
         <div>
             <p>This is the Login page</p>
@@ -28,10 +61,11 @@ function Login() {
                 />
             </div>
             <div>
-                <button>Login</button>
-                <button onClick={() => navigate('/register')}> Register </button>
+                <button onClick={login}>Login</button>
+                <button onClick={() => navigate('/register')}> Register</button>
                 <button>Forget Password</button>
             </div>
+            <label> {showErrorCode()}</label>
         </div>
     );
 }
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import './Register.css';
+import axios from "axios";
 
 function Register() {
     const [name, setName] = useState("");
@@ -10,8 +11,27 @@ function Register() {
     const [passwordConfirm, setPasswordConfirm] = useState("");
     const [email, setEmail] = useState("");
     const [jobTitle, setJobTitle] = useState("");
+    const [errorCode, setErrorCode]= useState(-1);
+
     const navigate = useNavigate();
 
+    const SERVER_URL = "http://localhost:8080"
+    const INVALID_REPEAT_PASSWORD = 102;
+    const USERNAME_NOT_AVAILABLE = 103
+
+    function register(){
+        axios.get(SERVER_URL+"/register?userName="+username + "&password=" + password
+            +"&name="+name+ "&lastName="+lastName+"&email=" + email +"&role="+ jobTitle)
+            .then(response => {
+                if (response.data.success){
+                    if (!response.data.registeredSuccessfully){
+                        setErrorCode(USERNAME_NOT_AVAILABLE)
+                    }else{
+                        navigate("/login");
+                    }
+                }
+            })
+    }
     const allFieldsFilled = () => {
         return (
             name.trim() &&
@@ -41,41 +61,57 @@ function Register() {
     function handleRegister(event) {
         event.preventDefault(); // מונע רענון של הדף
         if (password !== passwordConfirm) {
-            alert("Passwords do not match!");
+            setErrorCode(INVALID_REPEAT_PASSWORD);
         } else {
-            alert("Registration successful");
-            navigate("/login");
+            register();
         }
     }
 
+    function showErrorCode(){
+
+        let errorMessage = "";
+        switch (errorCode){
+
+            case -1 : errorMessage = "Please fill in all fields"; break;
+            case  USERNAME_NOT_AVAILABLE :errorMessage = "Username not available";break;
+            case INVALID_REPEAT_PASSWORD : errorMessage ="Invalid repeat password ";break;
+        }
+        return errorMessage;
+    }
 
     return (
-        <div className="register-form">
+        <div>
             <h2>Register Page</h2>
-            <form onSubmit={handleRegister}>
-                {/* Form fields using getInput */}
-                {getInput("Name", name, setName)}
-                {getInput("Last Name", lastName, setLastName)}
-                {getInput("Username", username, setUsername)}
-                {getInput("Password", password, setPassword, "password")}
-                {getInput("Password Confirm", passwordConfirm, setPasswordConfirm, "password")}
-                {getInput("Email", email, setEmail, "email")}
-                <div>
-                    <label>Job Title:</label>
-                    <select value={jobTitle} onChange={(e) => setJobTitle(e.target.value)}>
-                        <option value="" disabled>Select Job Title</option>
-                        <option value="Student">Student</option>
-                        <option value="Crew">Crew</option>
-                    </select>
-                </div>
+            <div className="register-form">
+                <form onSubmit={handleRegister}>
+                    {/* Form fields using getInput */}
+                    {getInput("Name", name, setName)}
+                    {getInput("Last Name", lastName, setLastName)}
+                    {getInput("Username", username, setUsername)}
+                    {getInput("Password", password, setPassword, "password")}
+                    {getInput("Password Confirm", passwordConfirm, setPasswordConfirm, "password")}
+                    {getInput("Email", email, setEmail, "email")}
+                    <div>
+                        <label>Job Title:</label>
+                        <select value={jobTitle} onChange={(e) => setJobTitle(e.target.value)}>
+                            <option value="" disabled>Select Job Title</option>
+                            <option value="Student">Student</option>
+                            <option value="Crew">Crew</option>
+                        </select>
+                    </div>
 
-                <button type="submit" className={allFieldsFilled() ? "active" : ""} disabled={!allFieldsFilled()}>
-                    Register
-                </button>
-            </form>
+                    <button type="submit"
+                            className={allFieldsFilled() ? "active" : ""}
+                            disabled={!allFieldsFilled()}>
+                        Register
+                    </button>
+
+                    <label> {showErrorCode()}</label>
+                </form>
+            </div>
         </div>
+
     );
 }
 
 export default Register;
-
