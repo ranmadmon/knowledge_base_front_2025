dd64d9087fea302bd6d8d2e328072ded4ac1df25
Aviya Yechezkel <aviyay@edu.acc.ac.il>
2024-12-03 16:53:51 +0200

    add getMaterials

--- src/pages/Course.jsx
+++ src/pages/Course.jsx
@@ -10,18 +10,28 @@ export default function Course() {
     const location = useLocation();
     const [courseID, setCourseID] = useState(location.pathname.split("/")[2]);
     const [courseData, setCourseData] = useState({});
+    const SERVER_URL = "http://localhost:8080"
 
+    // /get-materials-by-course-id
 
+    function getMaterials(){
+        axios.get(SERVER_URL+"get-materials-by-course-id?courseId="+courseID)
+            .then(response => {
+                console.log(response.data);
+            })
+    }
 
     useEffect(() => {
         const fetchData = async () => {
             setCourseData(await getCourse(courseID));
         }
         fetchData()
+        getMaterials()
     }, []);
     return (
         <div>
             <h1>
+                {console.log(courseData.name)}
                 {courseData.name +"  "+courseData.description+"  by:" + courseData?.lecturerEntity?.name}
             </h1>
             <div>
