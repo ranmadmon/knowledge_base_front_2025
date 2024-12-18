4698e99aaa521cfe8ccc6545ec31ceb157a04fb8
lior <liorb014@gmail.com>
2024-12-18 08:42:46 +0200

    table in dashboard adjustments

--- src/API/MaterialsAPI.jsx
+++ src/API/MaterialsAPI.jsx
@@ -1,10 +1,10 @@
 import axios from "axios"
 import * as Constants from "../Utils/Constants.jsx";
 
-export async function getMaterials() {
+export async function getMaterialsHistory() {
     try {
         //TODO  get-material-history היי חברי הצוות של ליאור בשביל למשוך מהשרת את המטיראל של היוזר יש להשתמש בנתיב -
-        const response = await axios.get(Constants.URL+"/get-materials", {})
+        const response = await axios.get(Constants.URL+"/get-materials-history", {})
         console.log(response)
         return await response?.data;
 
