d1c3f14b018b90297360e27d64bb07d928e045c0
lior <liorb014@gmail.com>
2024-12-29 23:11:06 +0200

    Changes by ramre

---
+++ src/Components/DownloadFiles.jsx
@@ -0,0 +1,42 @@
+import React, {useEffect, useState} from 'react';
+import {Card, Divider, Stack} from "@mui/material";
+import {getMaterialFiles} from "../API/FilesAPI.js";
+import DownloadIcon from '@mui/icons-material/Download';
+
+function DownloadFiles() {
+    const SUCCESS = "success"
+    const ERROR = "error"
+    const [cloudFiles, setCloudFiles] = useState([]);
+    const [materialId, setMaterialId] = useState(location.pathname.split("/")[4]);
+
+    async function handleGetMaterialFiles() {
+        const response = await getMaterialFiles(materialId)
+        setCloudFiles(response)
+    }
+
+    useEffect(() => {
+        handleGetMaterialFiles()
+    }, []);
+
+
+
+    return (
+        <Card sx={{
+            width: "100%",
+            height: "60vh",
+            marginTop: 2,
+            minHeight: "100%",
+            maxHeight: "100%",
+            maxWidth: "100%",
+            minWidth: "100%",
+        }}
+              elevation={6}>
+            <Stack sx={{height: "100%"}} direction="row"
+                   divider={<Divider orientation="vertical" flexItem sx={{margin: 2}}/>}
+            >
+            </Stack>
+        </Card>
+    );
+}
+
+export default DownloadFiles;
