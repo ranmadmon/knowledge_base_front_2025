ae27c278c756d2a78a7f74ddc742ce0711f22276
Aviya Yechezkel <aviyay@edu.acc.ac.il>
2024-12-30 04:21:09 +0200

    add by aviya and dvir and ram and lior

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -40,7 +40,7 @@ function Login() {
                 }else{
                     console.log(response.data);
                     setShowOtpComponent(true);
-                    
+
                 }
             })
     }
